9969f74a52d4b4201535471c1122063a7dbaa75c
Miguel Corrales <miguelangel.corrales@gmail.com>
2022-03-19 08:03:58 +0100

    Add dummy junit5

--- src/test/java/com/macorrales/replit/AppTest.java
+++ src/test/java/com/macorrales/replit/AppTest.java
@@ -1,10 +1,14 @@
 package com.macorrales.replit;
 
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class AppTest
 {
     /**
      * Rigorous Test :-)
